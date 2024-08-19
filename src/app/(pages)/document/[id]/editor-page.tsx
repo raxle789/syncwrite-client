e3699c38bee7a321a8008f68ec432c99fa2c997e
raxle789 <isqdifatih182@gmail.com>
2024-08-19 20:21:33 +0700

    fix: benerin error

--- src/app/(pages)/document/[id]/editor-page.tsx
+++ src/app/(pages)/document/[id]/editor-page.tsx
@@ -139,10 +139,7 @@ const EditorPage = () => {
 
   // useEffect
   useEffect(() => {
-    const s: Socket = io("https://syncwrite-server.vercel.app", {
-      transports: ["websocket"],
-      withCredentials: true,
-    });
+    const s: Socket = io("http://syncwrite-server.vercel.app");
     // const socket = io("https://your-server-domain.com", {
     //   transports: ["websocket"],
     //   withCredentials: true,
